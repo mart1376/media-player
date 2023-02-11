a00cafc7abf48aa78be6f1940747150b8b70b885
Nicolas Martichenko <mart1376@algonquinlive.com>
2023-02-10 21:36:50 -0500

    Hybrid-phase-2

--- js/app.js
+++ js/app.js
@@ -1,43 +1,36 @@
-import MEDIA from './data.js'; //the data file import
+import MEDIA from './data.js'; 
 
 const APP = {
-    audio: new Audio(), //the Audio Element that will play every track
-    currentTrack: 0, //the integer representing the index in the MEDIA array
-    // tracks: MEDIA.track,
+    audio: new Audio(), 
+    currentTrack: 0, 
     init: () => {
-        //called when DOMContentLoaded is triggered
         APP.addListeners();
         APP.addAudioListeners();
+        APP.load();
+        APP.buildPlaylist();
     },
     addListeners: () => {
-        //add event listeners for interface elements
-        document.getElementById('btnLoad').addEventListener('click', APP.load)
+        document.getElementById('track__item').addEventListener('click', APP.load)
         document.getElementById('btnPlay').addEventListener('click', APP.play)
         document.getElementById('btnPause').addEventListener('click', APP.pause)
         document.getElementById('btnNext').addEventListener('click', APP.next)
         document.getElementById('btnPrev').addEventListener('click', APP.prev)
         document.querySelector('.progress').addEventListener('click', APP.seek)
+        // document.getElementById('playPause').addEventListener('click', APP.playPause)
 
         window.addEventListener('visibilitychange', function(){
             if(document.visibilityState === 'visible'){
-                //entering page
                 if(APP.wasPlaying){
                     APP.play();
                 }
             }else{
-                //leaving page
                 APP.wasPlaying = !APP.audio.paused;
                 APP.pause(); 
             }
         });
     },
     addAudioListeners: () => {
-        //add event listeners for APP.audio
-        // APP.audio.addEventListener('error', APP.audioError)
-        // APP.audio.addEventListener('ended', APP.ended)
-        // APP.audio.addEventListener('loadStart', APP.loadStart)
-        // APP.audio.addEventListener('loadedMetaData', APP.loadedMetaData)
-        // APP.audio.addEventListener('canPlay', APP.canPlay)
+        APP.audio.addEventListener('error', APP.audioError);
         APP.audio.addEventListener('durationchange', APP.durationchange)
         APP.audio.addEventListener('timeupdate', APP.timeupdate)
         APP.audio.addEventListener('play', APP.play)
@@ -49,7 +42,6 @@ const APP = {
         andPlay && !(andPlay instanceof Event) && APP.play();
     },
     play: () => {
-        //start the track loaded into APP.audio playing
         if(APP.audio.src){
             APP.audio.play(); 
         }else{
@@ -57,8 +49,19 @@ const APP = {
         }
     },
     pause: () => {
-        //pause the track loaded into APP.audio playing
         APP.audio && APP.audio.pause();
+    },
+    playPause:() =>{
+        const playPause = document.getElementById('playPause').addEventListener('click', APP.playPause);
+        let isPlaying = false;
+            if(isPlaying){
+                playPause.innerHTML = `<button id="playPause" title="play | pause"><i class="material-symbols-rounded ms-controls">play_arrow</i></button>`;
+                isPlaying = false;
+            }else {
+                playPause.innerHTML = `<button id="playPause" title="play | pause"><i class="material-symbols-rounded ms-controls">pause</i></button>`;
+                isPlaying = true;
+            }
+
     },
     next: () =>{
         APP.audio.pause();
@@ -69,26 +72,57 @@ const APP = {
     prev: () =>{
         APP.audio.pause();
         APP.currentTrack --;
-        if(APP.currentTrack <= MEDIA.length) APP.currentTrack = 0; 
+        if(APP.currentTrack < 0) APP.currentTrack = 0; 
         APP.load(true);
     },
     ended: () =>{
         APP.next();
     },
     buildPlaylist: () => {
-        //read the contents of MEDIA and create the playlist
+        const albumCover = document.querySelector('.album_art__full img');
+        playlist.innerHTML = MEDIA.map(song => {
+            return `
+            <li class="track__item id="track__item">
+                <div class="track__thumb">
+                    <img src="${song.thumbnail}" alt="" />
+                </div>
+                <div class="track__details">
+                    <p class="track__title">${song.title}</p>
+                    <p class="track__artist">${song.artist}</p>
+                </div>
+                <div class="track__time">
+                    <time datetime="">${song.time}</time>
+                </div>
+            </li>`
+        }).join(' ');
+        //Album cover
+        albumCover.src=`${MEDIA[APP.currentTrack].large}`;
+        APP.loadCurrentTrack();
+
     },
     loadCurrentTrack: () => {
-        //use the currentTrack value to set the src of the APP.audio element
+        document.querySelector('li').addEventListener('click', APP.load(MEDIA[APP.currentTrack].track));
+
+        //Apply CSS for selected track
+        const selectedTrack = document.querySelectorAll('.playlist li')[APP.currentTrack];
+        selectedTrack.classList.add('selectedTrack');
     },
     convertTimeDisplay: (seconds) => {
-        //convert the seconds parameter to `00:00` style display
+        let minutes = Math.floor(seconds / 60);
+        let remainingSeconds = (seconds % 60).toFixed(0);
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        if (remainingSeconds < 10) {
+            remainingSeconds = "0" + remainingSeconds;
+        }
+        return minutes + ":" + remainingSeconds;
     },
     durationchange: (ev) =>{
-        document.getElementById('total-time').textContent = APP.audio.duration; 
+        document.getElementById('total-time').textContent = APP.convertTimeDisplay(APP.audio.duration); 
     },
     timeupdate: (ev) =>{
-        document.getElementById('current-time').textContent = APP.audio.currentTime;
+        document.getElementById('current-time').textContent = APP.convertTimeDisplay(APP.audio.currentTime);
         APP.showPct ();
     },
     showPct: () =>{
@@ -98,8 +132,7 @@ const APP = {
         let w = parseInt(document.querySelector('.progress').clientWidth);
         document.querySelector('.progress .played').getElementsByClassName.width = `${pct * w}px`;
     },
-    seek: () =>{ //Continue with yt video at 34min if there is time)
-
+    seek: () =>{
     },
 };
 
@@ -107,8 +140,5 @@ document.addEventListener('DOMContentLoaded', APP.init);
 
 /*
 TODO: 
-Load specific tracks when clicked
-Load Image of specific track
 Play/Pause button switch back n forth
-Change time to 00:00 format
 */
